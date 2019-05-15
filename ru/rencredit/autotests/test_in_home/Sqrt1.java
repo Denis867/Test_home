57f2de8c02d39e587766e066a094297909f9dee5
Trancer <ftrancerden@gmail.com>
2019-05-15 20:19:03 +0300

    Добавил класс Палиндром (+ комментарии к нему)

---
+++ ru/rencredit/autotests/test_in_home/Sqrt1.java
@@ -0,0 +1,9 @@
+package ru.rencredit.autotests.test_in_home;
+
+public class Sqrt1 {
+    public static void main (String [] args) {
+        int i = 16;
+        System.out.println("Корень квадратный из " + i + " = " + Math.round(Math.sqrt(i)));
+    }
+
+   }
