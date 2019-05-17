d2e9f3c7a9a264714787d0f80b16360be900c0a7
Trancer <ftrancerden@gmail.com>
2019-05-17 21:27:33 +0300

    Написан тест для Палиндрома

--- src/main/tests/ru/rencredit/autotests/tests/PalindromTest.java
+++ src/main/tests/ru/rencredit/autotests/tests/PalindromTest.java
@@ -1,12 +1,19 @@
 package ru.rencredit.autotests.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.rencredit.autotests.tasks.Palindrom;
 
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 import static org.testng.Assert.*;
 
 public class PalindromTest {
 
+    private Palindrom palindrom = new Palindrom();
+
     @Test
-    public void testMain() {
+    public void testPalindrom() {
+        Assert.assertEquals("madam", palindrom.palindromix(toLowerCase("MaDaM")));
+
     }
 }
